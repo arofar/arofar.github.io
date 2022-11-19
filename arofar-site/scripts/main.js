3bdf8eec47af055922e15b653f2f97fdc28335e3
arofar <65150466+arofar@users.noreply.github.com>
2022-11-19 11:00:46 +0800

    Add files via upload

---
+++ arofar-site/scripts/main.js
@@ -0,0 +1,7 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+  $(document).ready(function(){
+    $("p:eq(1)").css("background-color","yellow");
+  });
+ 
