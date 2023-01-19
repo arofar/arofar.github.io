1c602fe83b6bbd3bef552a32f3e65cfa38b9aa6b
arofar <65150466+arofar@users.noreply.github.com>
2023-01-19 21:01:44 +0800

    Delete arofar-site directory

--- arofar-site/scripts/main.js
+++
@@ -1,7 +0,0 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
-  $(document).ready(function(){
-    $("p:eq(1)").css("background-color","yellow");
-  });
- 
